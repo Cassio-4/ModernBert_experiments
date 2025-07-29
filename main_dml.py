959ed55898a5bd4c0d0731586a8f83427dde1a73
Cassio-4 <cassiobertasinascimento@gmail.com>
2025-07-29 14:41:00 -0400

    dml test loop working

--- main_dml.py
+++ main_dml.py
@@ -10,14 +10,12 @@ import pandas as pd
 import torch
 import torch.optim as optim
 from transformers import AutoTokenizer, AutoModel
-from pytorch_metric_learning import distances, losses, reducers
-from dml.losses_factory import MinerWrapper
+from dml.losses_factory import DML_LossesWrapper
 from datasets_utils import load_and_preprocess_dataset, unpack_dataset_info, get_dataloaders, SplitInstanceCollate, NerSlidingWindowReconstructor
 from dml.test import NERMetricsCalculator
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 print("Using device:", device)
-hf_tokenizer = AutoTokenizer.from_pretrained("microsoft/deberta-v3-base")
 
 def reshape_embeddings_and_labels(embeddings, labels):
     # Make labels 1D
@@ -33,7 +31,7 @@ def reshape_embeddings_and_labels(embeddings, labels):
         f"emb_reshaped_masked.shape: {emb_reshaped_masked.shape}, labels_masked.shape: {labels_masked.shape}"
     return emb_reshaped_masked, labels_masked
 
-def forward_one_epoch(model, train_loader, optimizer, loss_func, mining_func, reconstructor, train=True):
+def forward_one_epoch(model, train_loader, optimizer, dml_loss, reconstructor, train=True):
     if train:
         model.train()
     else:
@@ -44,14 +42,12 @@ def forward_one_epoch(model, train_loader, optimizer, loss_func, mining_func, re
         for batch_idx, (data, instance_ids) in enumerate(train_loader):
             input_ids = data['input_ids'].to(device)
             attention_mask = data['attention_mask'].to(device)
-            #labels = data['labels'].to(device)
             if train:
                 optimizer.zero_grad()
             embeddings = model(input_ids=input_ids, attention_mask=attention_mask).last_hidden_state
             recon_batch = reconstructor.reconstruct_sequences(embeddings, instance_ids, data)
             emb_masked, labels_masked = reshape_embeddings_and_labels(recon_batch["embeddings"], recon_batch["labels"])
-            indices_tuple = mining_func(emb_masked, labels_masked)
-            loss = loss_func(emb_masked, labels_masked, indices_tuple)
+            loss = dml_loss(emb_masked, labels_masked)
             if train:
                 loss.backward()
                 optimizer.step()
@@ -60,76 +56,50 @@ def forward_one_epoch(model, train_loader, optimizer, loss_func, mining_func, re
     mean_loss = total_loss / num_batches if num_batches > 0 else 0.0
     return mean_loss
 
-def train(config):
+def train(config, model, train_loader, val_loader, optimizer, loss_func, reconstructor, results_dir):
     EPOCHS = config['num_epochs']
     PATIENCE = config['patience']
-    for dataset in config['datasets']:
-        # Model
-        model = AutoModel.from_pretrained("microsoft/deberta-v3-base")
-        print("hf_model:", model)
-        # Load and preprocess the dataset
-        collate_fn = SplitInstanceCollate(hf_tokenizer, max_length=config["max_seq_length"], overlap=config["overlap"])
-        ds_info_dict, train_ds_name, valid_ds_name, n_labels = unpack_dataset_info(dataset)
-        tokenized_aligned_dataset, labels_list, id2label, label2id = load_and_preprocess_dataset(ds_info_dict, hf_tokenizer, config)
-        train_loader, val_loader, _ = get_dataloaders(tokenized_aligned_dataset, config, ["train", "val"], 
-                                    collate_fn=collate_fn, select=config.get("select", -1))
-        reconstructor = NerSlidingWindowReconstructor(hf_tokenizer, config["overlap"])
+    print("Starting training...")
+    #val_metrics = {"f1_score": [], "accuracy_score": [], "recall_score": []}
+    train_loss, val_loss = [], []
+    best_val_loss = float('inf')
+    patience_counter = 0
+    model_save_name = os.path.join(results_dir, "best_model.pth")
+    results_save_name = os.path.join(results_dir, "results.csv")
+
+    for epoch in range(1, EPOCHS + 1):
+        # Train epoch
+        epoch_avg_loss = forward_one_epoch(model, train_loader, optimizer, loss_func, reconstructor)
+        train_loss.append(epoch_avg_loss)
+        # Validation epoch
+        val_avg_loss = forward_one_epoch(model, val_loader, optimizer, loss_func, reconstructor, train=False)
+        val_loss.append(val_avg_loss)
+        # Calculate metrics
+        #metrics = test(train_loader, val_loader, model, accuracy_calculator)
+        #val_metrics["f1_score"].append(metrics["f1_score"])
+        #val_metrics["accuracy_score"].append(metrics["accuracy_score"])
+        #val_metrics["recall_score"].append(metrics["recall_score"])
+        print("Epoch {}: train_loss: {:.4f}, val_loss: {:.4f}".format(epoch, train_loss[-1], val_loss[-1]))
+        if val_avg_loss < best_val_loss:
+            best_val_loss = val_avg_loss
+            patience_counter = 0
+            torch.save(model.state_dict(), model_save_name)
+            print(f"Saved best model with val_loss: {best_val_loss:.4f}")
+        else:
+            patience_counter += 1
+            if patience_counter >= PATIENCE:
+                print(f"Early stopping at epoch {epoch}, no improvement in {PATIENCE} epochs.")
+                break
     
-        model = model.to(device)
-        optimizer = optim.Adam(model.parameters(), lr=config['learning_rate'])
-
-        ### pytorch-metric-learning stuff ###
-        distance = distances.CosineSimilarity()
-        reducer = reducers.ThresholdReducer(low=0)
-        mining_func = MinerWrapper(miner_name="TripletMarginMiner", margin=0.2, distance=distance, 
-                                   type_of_triplets="semihard")
-        loss_func = losses.TripletMarginLoss(margin=0.2, distance=distance, reducer=reducer)
-        accuracy_calculator = NERMetricsCalculator(include=("accuracy_score", "f1_score", "recall_score"), k=3)
-    
-        print("Starting training...")
-        val_metrics = {"f1_score": [], "accuracy_score": [], "recall_score": []}
-        train_loss, val_loss = [], []
-        best_val_loss = float('inf')
-        patience_counter = 0
-
-        for epoch in range(1, EPOCHS + 1):
-            # Train epoch
-            epoch_avg_loss = forward_one_epoch(model, train_loader, optimizer, loss_func, mining_func, reconstructor)
-            train_loss.append(epoch_avg_loss)
-            # Validation epoch
-            val_avg_loss = forward_one_epoch(model, val_loader, optimizer, loss_func, mining_func, reconstructor, train=False)
-            val_loss.append(val_avg_loss)
-            # Calculate metrics
-            metrics = test(train_loader, val_loader, model, accuracy_calculator)
-            val_metrics["f1_score"].append(metrics["f1_score"])
-            val_metrics["accuracy_score"].append(metrics["accuracy_score"])
-            val_metrics["recall_score"].append(metrics["recall_score"])
-            print("Epoch {}: train_loss: {:.4f}, val_loss: {:.4f}, val_f1: {:.4f} ".format(
-                epoch, train_loss[-1], val_loss[-1], val_metrics["f1_score"][-1]))
-            if val_avg_loss < best_val_loss:
-                best_val_loss = val_avg_loss
-                patience_counter = 0
-                torch.save(model.state_dict(), "best_deberta_dml_model.pth")
-                print(f"Saved best model with val_loss: {best_val_loss:.4f}")
-            else:
-                patience_counter += 1
-                if patience_counter >= PATIENCE:
-                    print(f"Early stopping at epoch {epoch}, no improvement in {PATIENCE} epochs.")
-                    break
-        _, _, test_loader = get_dataloaders(tokenized_aligned_dataset, config, ["test"], 
-                                    collate_fn=collate_fn, select=config.get("select", -1))
-        test_prec = test(train_loader, test_loader, model, accuracy_calculator)
-
-        print(f"Final test precision: {test_prec}")
-        results_df = pd.DataFrame({
-            "epoch": list(range(1, len(train_loss) + 1)),
-            "train_loss": train_loss,
-            "val_loss": val_loss,
-            "val_f1_score": val_metrics["f1_score"],
-            "val_accuracy_score": val_metrics["accuracy_score"],
-            "val_recall_score": val_metrics["recall_score"]
-        })
-        results_df.to_csv("training_results.csv", index=False)
+    results_df = pd.DataFrame({
+        "epoch": list(range(1, len(train_loss) + 1)),
+        "train_loss": train_loss,
+        "val_loss": val_loss
+        #"val_f1_score": val_metrics["f1_score"],
+        #"val_accuracy_score": val_metrics["accuracy_score"],
+        #"val_recall_score": val_metrics["recall_score"]
+    })
+    results_df.to_csv(results_save_name, index=False)
 
 def get_all_embeddings(loader: torch.utils.data.DataLoader, model):
     embeddings_lst = []
@@ -157,11 +127,55 @@ def test(train_loader, test_loader, model, accuracy_calculator):
     )
     return accuracies
 
-def run_experiments(configs_paths_lst: list):
+def run_experiments(config):
+    for dataset in config['datasets']:
+        for loss_name in config['losses']:
+            experiment_name = f"{dataset}_{loss_name}"
+            print(f"Running experiment: {experiment_name}")
+            results_dir = os.path.join("./dml/results", experiment_name)
+            os.makedirs(results_dir, exist_ok=True)
+            # Model
+            model = AutoModel.from_pretrained(config['model_checkpoint'])
+            model = model.to(device)
+            # Tokenizer
+            hf_tokenizer = AutoTokenizer.from_pretrained(config['model_checkpoint'])
+            # Load and preprocess the dataset
+            collate_fn = SplitInstanceCollate(hf_tokenizer, max_length=config["max_seq_length"], overlap=config["overlap"])
+            ds_info_dict, train_ds_name, valid_ds_name, n_labels = unpack_dataset_info(dataset)
+            tokenized_aligned_dataset, labels_list, id2label, label2id = load_and_preprocess_dataset(ds_info_dict, hf_tokenizer, config)
+            train_loader, val_loader, test_loader = get_dataloaders(tokenized_aligned_dataset, config, ["train", "val", "test"], 
+                                        collate_fn=collate_fn, select=config.get("select", -1))
+            reconstructor = NerSlidingWindowReconstructor(hf_tokenizer, config["overlap"])
+            # optimizer
+            optimizer = optim.Adam(model.parameters(), lr=config['learning_rate'])
+
+            # Pytorch Metric Learning setup
+            #mining_func = MinerWrapper(miner_name="TripletMarginMiner", )miner_name="TripletMarginMiner",
+            loss_func = DML_LossesWrapper(loss_name=loss_name, max_samples=1500)
+            K = 3
+            accuracy_calculator = NERMetricsCalculator(include=("accuracy_score", "f1_score", "recall_score"), k=K)
+            # TRAIN
+            train(config, model, train_loader, val_loader, optimizer, loss_func, reconstructor, results_dir)
+            # Load best model
+            print(f"Loading best model from {results_dir}.pth")
+            model.load_state_dict(torch.load(os.path.join(results_dir, "best_model.pth"), map_location=device))
+            # TEST
+            test_metrics = test(train_loader, test_loader, model, accuracy_calculator)
+            print(f"Final test precision: {test_metrics}")
+            # Write config and test_metrics to a .txt file
+            summary_path = os.path.join(results_dir, f"summary_K{K}.txt")
+            with open(summary_path, "w") as f:
+                f.write("CONFIGURATION:\n")
+                f.write(json.dumps(config, indent=2))
+                f.write("\n\nTEST METRICS:\n")
+                for k, v in test_metrics.items():
+                    f.write(f"{k}: {v}\n")
+
+def run_configs(configs_paths_lst: list):
     for config_path in configs_paths_lst:
             with open(config_path, 'r') as file:
                 config = json.load(file)
-            train(config)
+            run_experiments(config)
 
 if __name__ == "__main__":
     # Run specific config file if user specifies it
@@ -192,7 +206,7 @@ if __name__ == "__main__":
                 config_files_lst.append(os.path.join("dml/configs/", f))
     
     print(f"configs to run: {config_files_lst}")
-    run_experiments(config_files_lst)
+    run_configs(config_files_lst)
 
 """
 if __name__ == "__main__":
